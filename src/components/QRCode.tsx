f9fad45caffbae69c72aed924839201e1a014641
gourab <biz.gourab@gmail.com>
2022-05-27 15:48:34 +0530

    responsive screen removed

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import QR from 'react-native-qrcode-svg'
-import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export type Props = {
     value: string;
@@ -11,14 +10,12 @@ export type Props = {
 
 const QRCode = ({
      value = '', 
-     size = hp( '25%' ), 
      title = ''
      }: Props) => {
   return (
     <View style={styles.containerQrCode}>
       <QR 
          value={value}
-         size={size}
       />
       {
         title !== '' && (
@@ -34,12 +31,10 @@ export default QRCode
 const styles = StyleSheet.create({
     containerQrCode: {
         backgroundColor: '#e3e3e3',
-        height: hp( '30%' ),
         justifyContent: 'center',
         marginLeft: 20,
         marginRight: 20,
         alignItems: 'center',
-        marginTop: hp( '3%' )
     },
       textQr: {
         color: '#6c6c6c',
