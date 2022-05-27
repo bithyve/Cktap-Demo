ccc32cfa15218037877a667d4f1acf0164155d3d
PraneethGunas <praneethgunas404@gmail.com>
2022-05-27 16:25:36 +0530

    clean up

--- index.js
+++ index.js
@@ -5,8 +5,7 @@
 import './shim';
 
 import App from './App';
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 
-require('cbor-rn-prereqs');
 AppRegistry.registerComponent(appName, () => App);
