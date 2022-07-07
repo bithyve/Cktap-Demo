f537d6a36dd57d8dbc1a74a0101abd881351aff1
PraneethGunas <praneethgunas404@gmail.com>
2022-07-07 17:45:11 +0530

    Added navigation and svg support

--- declare.d.ts
+++ declare.d.ts
@@ -1,1 +1,4 @@
 declare module 'coinkite-tap-protocol-js';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,26 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const { getDefaultConfig } = require("metro-config");
 
-module.exports = {
-  resolver: {
-    extraNodeModules: {
-      stream: require.resolve('stream-browserify'),
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
     },
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
+    resolver: {
+      extraNodeModules: {
+        stream: require.resolve("stream-browserify"),
       },
-    }),
-  },
-};
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"],
+    },
+  };
+})();
